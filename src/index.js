daffa2880959fe6f21c8a2f6f776a301bb2e4a93
Ojo Oluwasetemi <Oluwasetemi@users.noreply.github.com>
2018-05-25 11:39:51 +0100

    Completed Day10
    More on Parent and Child composition

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import { render } from "react-dom";
 // import Day6 from './Day6';
 // import Day7 from './Day7';
 // import Day8 from './Day8';
-import Day9 from './Day9';
+// import Day9 from './Day9';
+import Day10 from './Day10';
 
 const App = () => (
   <div>
-    <Day9 />
+    <Day10 />
   </div>
 );
 
